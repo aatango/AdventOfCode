8d6a774d7691bc2f0bd827487d549eed355b4e23
aatango <80591203+aatango@users.noreply.github.com>
2025-04-12 13:26:06 +0200

    Time daily solvers

--- 2024/Sources/main.cc
+++ 2024/Sources/main.cc
@@ -3,6 +3,7 @@
 #include "MullItOver.hh"
 #include "RedNosedReports.hh"
 
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iterator>
@@ -20,9 +21,15 @@ void solve(std::size_t const day, std::string_view const name, auto solverFuncti
     auto inputData = std::string { std::istreambuf_iterator<char> { inputStream },
         std::istreambuf_iterator<char> {} };
 
+    auto const startTime = std::chrono::steady_clock::now();
+
     auto const [firstResult, secondResult] = solverFunction(std::move(inputData));
 
-    std::println("Day {:02}, {}: {} | {}", day, name, firstResult, secondResult);
+    auto const endTime = std::chrono::steady_clock::now();
+    auto const duration
+        = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+    std::println("Day {:02}, {}: {} | {} ({})", day, name, firstResult, secondResult, duration);
 }
 
 } // namespace
